f804c405308d1ef354c2cc8c8d3f5402deb67005
CodingBash <bbecerr@outlook.com>
2018-07-19 10:15:40 -0400

    Added batch script to analyze COREs for a single tumorID - The parallelized script would have issues on some taskIDs, so this new script should help

---
+++ scripts/batchFacetsCoresDsync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#  specify that the job requires 16GB of memory
+#$ -l m_mem_free=16G
+
+# run commands and application
+source /sonas-hs/it/hpc/home/easybuild/lmod-setup.sh
+module use -a /sonas-hs/tuveson/hpc/home/software/modulefiles
+module load RBio/3.6.0
+
+pwd
+date
+Rscript tcgaFacetsCoreGenerationHPCJob.R D DtcgaCoreTableBP DtcgaCOREobjBP ${tumorID}
+date
+
